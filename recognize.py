689ff534aced8bf984da8797d527add649865688
Min-Uk.Lee <makerdark98@naver.com>
2018-09-15 13:06:51 +0900

    Fix recognize
    main function input npy_path and ndarray in form of list. So convert from list to ndarray & compare encodings

--- recognize.py
+++ recognize.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
 import face_recognition
 import numpy as np
 import os
 import time
+import ast
 
 known_face_encodings = []
 known_face_names = []
@@ -11,21 +13,26 @@ def read_files_in_dir(dir_path):
         for fname in files:
             image = np.load(dir_path + fname)
             known_face_encodings.append(image)
-            known_face_names.append(fname.replace('.npy',''))
-def matching_file(npy_file):
-    matches = face_recognition.compare_faces(known_face_encodings,npy_file,tolerance = 0.5)
-    name = "Unkown"
+            known_face_names.append(fname.replace('.npy', ''))
+
+
+def matching_file(encoding):
+    matches = face_recognition.compare_faces(known_face_encodings, encoding,tolerance = 0.5)
+    name = "Unknown"
+    distance = 0
 
     if True in matches:
         first_match_index = matches.index(True)
         name = known_face_names[first_match_index]
         distance = np.linalg.norm(known_face_encodings[first_match_index]-npy_file)
-        print (name,distance)
+
+    return (name,distance)
 
 
 if __name__ == "__main__":
-    read_files_in_dir('./encodings/')
-    print ("ready")
-    file_dir = input()
-    npy_image = np.load(file_dir)
-    matching_file(npy_image)
+    npy_path = input()
+    read_files_in_dir(npy_path)
+    data = np.array(ast.literal_eval(input()))
+    matching_result = matching_file(data);
+    print(matching_result[0])
+    print(matching_result[1])
